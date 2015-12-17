010f301ed31bc807ded51db26b53afd78849d2a2
Marquis Dennis <marquis103@gmail.com>
2015-12-17 12:45:24 -0500

    Added continous background
    	modified:   FlappyBirdClone/GameScene.swift

--- FlappyBirdClone/GameScene.swift
+++ FlappyBirdClone/GameScene.swift
@@ -10,8 +10,44 @@ import SpriteKit
 
 class GameScene: SKScene {
     var bird = SKSpriteNode()
+    var bg = SKSpriteNode()
     
     override func didMoveToView(view: SKView) {
+        //background texture
+        let bgTexture = SKTexture(imageNamed: "bg.png")
+        
+        //adding background texture to node
+        bg = SKSpriteNode(texture: bgTexture)
+        
+        //set bg position
+        bg.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
+        
+        bg.size.height = frame.height
+        
+        //move background to the left so bird appears to be moving right
+        let movebg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        //move background from the extreme left to the extreme right
+        let replacebg = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        
+        //want this to happen when background has moved all the way to the left
+        let movebgForever = SKAction.repeatActionForever(SKAction.sequence([movebg, replacebg]))
+        
+        //create new bg node to add to the scene
+        for var i:CGFloat=0; i < 3; i++ {
+            //adding background texture to node
+            bg = SKSpriteNode(texture: bgTexture)
+            
+            //x is the center of the background image
+            
+            bg.position = CGPoint(x: bgTexture.size().width / 2 + bgTexture.size().width * i, y: CGRectGetMidY(frame))
+            
+            bg.size.height = frame.height
+            
+            bg.runAction(movebgForever)
+            
+            self.addChild(bg)
+        }
+        
         //everything that appears on the screen is a node
         //assign flappy1 to sprite node
         //image used to display a sprite is a texture
@@ -34,7 +70,6 @@ class GameScene: SKScene {
         
         //add node to the screen
         self.addChild(bird)
-        
         
     }
     
